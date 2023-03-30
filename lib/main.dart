4071d725a5f92ed0c54bf0b2cc62a6bfe60dd5d2
mohamed ibrahem <mohamed.data13@gmail.com>
2023-03-30 05:46:34 +0200

    adding new images and text

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,74 @@ class MyApp extends StatelessWidget {
           title: const Text("Demo"),
           centerTitle: true,
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage:
-                    AssetImage('assets/images/man-avatar-profile-vector.jpg'),
-              )
-            ]),
+        body: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 2, color: Colors.black),
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 100,
+                  backgroundImage:
+                      AssetImage('assets/images/man-avatar-profile-vector.png'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage:
+                          AssetImage('assets/images/google-plus.png'),
+                    ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: AssetImage('assets/images/facebook.png'),
+                    ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage:
+                          AssetImage('assets/images/instagram.png'),
+                    ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: AssetImage('assets/images/linkedin.png'),
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(width: 1, color: Colors.deepPurple.shade600),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Text(
+                        "chromicle",
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "@fmFOSS",
+                        style:
+                            TextStyle(fontSize: 20, fontFamily: 'Montserrat'),
+                      ),
+                      Text(
+                        "Mobile App Developer and Open Source enthusiastic ",
+                        softWrap: true,
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ],
+                  ),
+                )
+              ]),
+        ),
       ),
     );
   }
