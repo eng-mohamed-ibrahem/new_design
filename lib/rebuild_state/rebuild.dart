3cdb998a4effdf4ba7d8f6dedcdf86df176ed044
mohamed ibrahem <mohamed.data13@gmail.com>
2023-04-01 07:40:13 +0200

    adding FontAwesomeIcons

---
+++ lib/rebuild_state/rebuild.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// create state object for myhome page statefulwidget
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  // call before build method
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _incrementCounter() {
+    setState(() {});
+  }
+}
--- lib/sites.dart
+++ lib/sites.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Sites extends StatelessWidget {
   const Sites({super.key});
@@ -12,22 +12,26 @@ class Sites extends StatelessWidget {
         CircleAvatar(
           radius: 30,
           backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('assets/images/google-plus.png'),
+          child: Icon(FontAwesomeIcons.googlePlus),
+          // backgroundImage: AssetImage('assets/images/google-plus.png'),
         ),
         CircleAvatar(
           radius: 30,
           backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('assets/images/facebook.png'),
+          child: Icon(FontAwesomeIcons.facebookF),
+          // backgroundImage: AssetImage('assets/images/facebook.png'),
         ),
         CircleAvatar(
           radius: 30,
           backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('assets/images/instagram.png'),
+          child: Icon(FontAwesomeIcons.instagram),
+          // backgroundImage: AssetImage('assets/images/instagram.png'),
         ),
         CircleAvatar(
           radius: 30,
           backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('assets/images/linkedin.png'),
+          child: Icon(FontAwesomeIcons.linkedin),
+          // backgroundImage: AssetImage('assets/images/linkedin.png'),
         ),
       ],
     );
